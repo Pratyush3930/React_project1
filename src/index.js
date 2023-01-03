9503c97d4d1596b65d732032e626dda3fda82fed
Pratyush <pratyushojha059@gmail.com>
2023-01-03 21:06:23 +0545

    Started REACT project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// above is used for importing packages 
+// 'package_name'
 
-import App from './App';
+import App from './App';//to import user defined files use './name'
 
 ReactDOM.render(<App />, document.getElementById('root'));
